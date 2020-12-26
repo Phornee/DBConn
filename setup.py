6161a6ab89807afd049f03d9193b3caaf9fe3a9b
Phornee <phornee@gmail.com>
2020-12-26 13:10:40 +0100

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DBConn-pkg-Phornee", # Replace with your own username
+    name="DBConn",
     version="0.0.1",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
