ace5011c68c96103b46146bed29f8c9020122eb5
Phornee <phornee@gmail.com>
2021-03-31 09:57:31 +0200

    influx

--- dbutils_phornee/influxdb_conn.py
+++ dbutils_phornee/influxdb_conn.py
@@ -1,5 +1,7 @@
 from .db_conn import DBConn, DBOpenException, DBGetLockException, DBReleaseLockException
-
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 class InfluxDBConn(DBConn):
 
@@ -7,13 +9,24 @@ class InfluxDBConn(DBConn):
         super().__init__()
 
     def openConn(self, params, autocommit=True):
-        self.conn.autocommit = autocommit
+        token = params['token']
+        self.org = params['org']
+        self.bucket = params['bucket']
+
+        self.conn = InfluxDBClient(url=params['url'], token=token)
 
     def closeConn(self):
         self.conn.close()
 
     def insert(self, table, params):
-        pass
+        write_api = self.conn.write_api(write_options=SYNCHRONOUS)
+
+        point = Point(table)\
+                .tag("host", "host1")\
+                .field("used_percent", 23.43234543)\
+                .time(datetime.utcnow(), WritePrecision.NS)
+
+        write_api.write(self.bucket, self.org, point)
 
     def getLock(self, lockname):
         raise
